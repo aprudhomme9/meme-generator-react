a326f7ec33c0b1785a4101310de2a84cf666757d
Andy Prudhomme <aprudhomme9@gmail.com>
2018-11-04 18:26:46 -0600

    can grab info from image with click

--- src/MemeList/index.js
+++ src/MemeList/index.js
@@ -8,20 +8,23 @@ class MemeList extends Component{
 			images: []
 		}
 	}
-
+	handleClick = (e) => {
+		console.log(e.currentTarget.name);
+	}
 	render(){
-		const image = this.props.images.map((image) => {
-			console.log(image.imageUrl);
+		const generatorList = this.props.images.map((image, i) => {
+			// console.log(image.imageUrl);g
 			return (
-					<img height='400' width='400' src={image.imageUrl} />
+					<img name={image.displayName} onClick={this.handleClick} key={i} height='400' width='400' src={image.imageUrl} />
 					
 			)
-			// console.log(image);
 		})
 
 		return(
 			<div>
-				{image}
+				<ul>
+					{generatorList}	
+				</ul>
 			</div>
 			
 
