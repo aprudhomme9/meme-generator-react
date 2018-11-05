c3d4a7d11c6cd6dd0dbd6129b1f780240ad8d899
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-05 12:57:46 -0600

    added search bar in header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 
-import {Button, Grid, Label, Header} from 'semantic-ui-react';
+import {Button, Grid, Label, Header, Input} from 'semantic-ui-react';
 
 class HeaderApp extends Component{
 	render(){
 		return(
 			<Header>
-
 				<nav>
-					<a href='#'>Profile</a><br/>
-					<a href='#'>Channels</a>
-				</nav>
+				<Grid columns={1} divided textAlign='left' style={{ height: '100%' }} verticalAlign='top' stackable>
+        	
+          	<Grid.Column>
+							<a href='#'>Profile</a><br/>
+							<a href='#'>Channels</a><br/>
+							<a href='#'>LeaderBoard</a><br/>
+							<Input type="text" placeholder="Search..."/>
+      		</Grid.Column>	
+      	
+      </Grid>
+			</nav>
+
+
 
 			</Header>
 
