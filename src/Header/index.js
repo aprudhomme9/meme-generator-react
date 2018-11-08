3ed95e023953b9eae8c7705d9f021dfad95d7f1c
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-08 17:13:46 -0600

    everything is broken

--- src/Header/index.js
+++ src/Header/index.js
@@ -19,15 +19,10 @@ class HeaderApp extends Component{
           	<Logout /> 
           	{this.props.isLoggedIn ? <Logout /> : <Link to='/login'>Login/Register</Link> }<br/>
           	<Link to='/register'>register</Link><br/>
-
           	<Link to='/home'>Home</Link><br/>
 						<Link to='/profile'>Profile</Link><br/>
-
 						<Link to='/leaderboard'>LeaderBoard</Link><br/>
 						<Link to='/users'>Meme Community</Link><br/>
-
-						<Link to='/leaderboard'>Fire Memes</Link><br/>
-						<Link to='/users'>Meme Community</Link>
       		</Grid.Column>	
       	</Grid>
 
